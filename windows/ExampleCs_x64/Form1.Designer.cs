b3266e15f900319272ea0e88838e04c279cb54d3
vohungvi <vohungvi27@gmail.com>
2020-05-14 12:02:35 +0700

    + add text

--- windows/ExampleCs_x64/Form1.Designer.cs
+++ windows/ExampleCs_x64/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,11 +52,21 @@
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(413, 324);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -71,6 +82,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- windows/ExampleCs_x64/Form1.cs
+++ windows/ExampleCs_x64/Form1.cs
@@ -21,36 +21,50 @@ namespace Example
         private void Form1_Load(object sender, EventArgs e)
         {
             //khởi tạo đối tượng
+            //Init class
             IPSScar detector = new IPSScar();
 
             //CÁC OPTION
 
             //đường dẫn chứa file output, nếu set null hoặc rỗng thì sẽ không save ảnh output
+            //set output dir, if output dir is null or empty engine will not save output image
             detector.OutputFoler = @"D:\Result";
            
 
             //engine nhận tham số là ảnh hoặc đường dẫn đến ảnh
+            //you can set input pameter is bitmap or image path
             CarPlate result = detector.ReadPlate(@"Test98.JPG");
 
             //kết quả trả về là class IPSSresult gồm nhiều thuộc tính
             //text là các ký tự biển số
+            //result is a class contain some values
+            //.text is plate character
             label1.Text = result.text;
 
             //bitmap là hình ảnh kết quả
+            //bimap is image has returned
             Bitmap bmp = result.bitmap;
             pictureBox1.Image = bmp;
 
             //thời gian xử lý
+            //elapsed time read plate
             int ms = result.elapsedMilisecond;
 
             //mã lỗi (nếu có)
+            //error code to diagnostic
             string error = result.error;
 
             //nếu có biển số giá trị là true
+            //.hasPlate is true when found plate in image
             bool hasPlate = result.hasPlate;
 
             //nếu đọc đủ các ký tự thì kết quả là true
-            bool isValid = result.isValid;            
+            //.isValid is true when read enough character
+            bool isValid = result.isValid;
+
+            //biển số vuông hay biển số dài
+            //.type is long or short plate 
+            label2.Text = result.type.ToString();
         }
     }
 }
